5a2967a0040b7c35a7dde633319eb03abcbb06a2
Dmitry <75564429+reeklama@users.noreply.github.com>
2022-03-29 13:54:40 +0700

    Update gradient_check.py

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -35,6 +35,12 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
         numeric_grad_at_ix = 0
 
         # TODO compute value of numeric gradient of f to idx
+        x0 = x.copy()
+        x1 = x.copy()
+        x0[ix]+=delta
+        x1[ix]-=delta
+        numeric_grad_at_ix=(f(x0)[0]-f(x1)[0])/(2*delta)
+        
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
